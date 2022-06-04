465faa1ecee224961899ae5b2d6588db2109be7b
danydecrush <76194638+danydecrush@users.noreply.github.com>
2022-06-04 20:05:54 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,16 +1,14 @@
-sudo apt-get install build-essential cmake
-sudo apt-get install libgtk-3-dev
-sudo apt-get install libboost-all-dev
+apt-get install build-essential cmake
+apt-get install libgtk-3-dev
+apt-get install libboost-all-dev
 
 mkdir -p ~/.streamlit/
 
-# shellcheck disable=SC2028
 echo "\
 [general]\n\
 email = \"your-email@domain.com\"\n\
 " > ~/.streamlit/credentials.toml
 
-# shellcheck disable=SC2028
 echo "\
 [server]\n\
 headless = true\n\
